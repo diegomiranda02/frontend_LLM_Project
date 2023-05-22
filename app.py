e6b173bf91097267c3bb6550f4b93f3415c21c4a
rpa <rpa@RPA.myguest.virtualbox.org>
2023-05-21 21:04:22 -0300

    Adicionando funcionalidades para visualizar em full screen e renderizar tabelas e textos

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # streamlit library
 import streamlit as st
-
+st. set_page_config(layout="wide") 
 # HTML requests library
 from requests_html import HTMLSession
 
 # basic libraries
 import time
+
+# JSON to render the API responde into a Python Object
+import json
+
+# Pandas
+import pandas as pd
+
 
 # start an HTML session to get page contents
 sess = HTMLSession()
@@ -24,13 +31,23 @@ command = st.text_input("O que deseja?", "Gerar um relatorio?", disabled=False)
 # check if the Send button was pressed and get the API Data
 if st.button("Enviar"):        
     with st.spinner('Consulta em andamento...'):
-        answer = get_data(command=command)
+        data = get_data(command=command)
+        data_content = json.loads(data)
 
-    # Print the API Response like it is being typed in real time
-    st.markdown("## Resposta")
-    st.markdown("---")
-    text = answer
-    t = st.empty()
-    for i in range(len(text) + 1):
-        t.markdown("## %s" % text[0:i])
-        time.sleep(0.04)
+        count = 0
+        for key,value in data_content.items():
+
+            if isinstance(value, str):
+                # Print text Streamlit
+                t = st.empty()
+                for i in range(len(value) + 1):
+                    t.markdown("## %s" % value[0:i])
+                    time.sleep(0.04)
+
+            if isinstance(value, list):
+                #print(value)
+                df_from_list = pd.DataFrame(value)
+                print(df_from_list)
+                st.table(df_from_list)
+
+            count += 1
